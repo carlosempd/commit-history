3577737b3ca91edfe6ca2f416edfd3999b0aba9a
Carlos <carlosempd@gmail.com>
2021-03-06 01:35:01 -0400

    Create routes, add some content in home component, add style to fade animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommitsComponent } from './components/commits/commits.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'commits', component: CommitsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
