dca93f6889cae55c9ff52c1b2ad7bd844d043a71
Carlos <carlosempd@gmail.com>
2021-03-06 21:26:28 -0400

    Add a search form to find commits by message

--- src/app/components/commits/commits.component.ts
+++ src/app/components/commits/commits.component.ts
@@ -8,7 +8,8 @@ import { GithubService } from 'src/app/services/github.service';
 })
 export class CommitsComponent implements OnInit {
 
-  commits: any[] = [];
+  allCommits: any[] = [];
+  searchCommits: any[] = [];
   loading = false;
 
   constructor(private githubService: GithubService) { 
@@ -17,13 +18,35 @@ export class CommitsComponent implements OnInit {
 
     this.githubService.getCommitHistory().subscribe( (data: any[]) => {
       
-      this.commits = data;
+      this.searchCommits = data;
+      this.allCommits = data;
       this.loading = false;
 
     });
   }
 
   ngOnInit(): void {
+  }
+
+  // Search commits
+  search( searchTerm: string ) {
+    
+    if ( searchTerm === '' ) {
+      this.searchCommits = this.allCommits;
+      return;
+    }
+
+    this.searchCommits = this.allCommits.filter( 
+      element => element.commit.message.toLowerCase().includes( searchTerm.toLowerCase() )
+    );
+  }
+
+  // Prevent Default when press Enter
+  checkEnter($event){
+    if ( $event.key === 'Enter') {
+      $event.preventDefault();
+
+    }
   }
 
 }
